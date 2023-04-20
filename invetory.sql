55b8868cb2c96d809a2b422061e813af573cf65d
Abentesfaye <abentesfaye11@gmail.com>
2023-04-21 00:31:11 +0300

    sql and readme

---
+++ invetory.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Apr 20, 2023 at 11:18 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.2.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `invetory`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `product`
+--
+-- Error reading structure for table invetory.product: #1932 - Table &#039;invetory.product&#039; doesn&#039;t exist in engine
+-- Error reading data for table invetory.product: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `invetory`.`product`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sells`
+--
+-- Error reading structure for table invetory.sells: #1932 - Table &#039;invetory.sells&#039; doesn&#039;t exist in engine
+-- Error reading data for table invetory.sells: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `invetory`.`sells`&#039; at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+-- Error reading structure for table invetory.users: #1932 - Table &#039;invetory.users&#039; doesn&#039;t exist in engine
+-- Error reading data for table invetory.users: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near &#039;FROM `invetory`.`users`&#039; at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
